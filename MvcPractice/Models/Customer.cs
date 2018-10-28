3d3f612055723106a06793c8afb7c07af95bfdb1
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2018-10-28 14:28:30 +0200

    Application gets data from Sql database

--- MvcPractice/Models/Customer.cs
+++ MvcPractice/Models/Customer.cs
@@ -6,5 +6,11 @@ namespace MvcPractice.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public bool isSubscribedToNewsLetter { get; set; }
+
+        public MemberShipType MemberShipType { get; set; }
+
+        public byte MemberShipTypeId { get; set; }
     }
 }
